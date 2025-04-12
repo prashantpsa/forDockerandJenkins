4c65fbca0535be900a7085081372c77804acbf35
prashantankushkani71@gmail.com <prashantankushkani71@gmail.com>
2025-04-12 09:23:46 +0530

    example

---
+++ src/main/java/com/postEx/Controller/PostEx.java
@@ -0,0 +1,18 @@
+package com.postEx.Controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/postEx")
+public class PostEx {
+
+    @GetMapping()
+    public ResponseEntity<String> postexample(){
+
+        return new ResponseEntity<>("controller",HttpStatus.OK);
+    }
+}
